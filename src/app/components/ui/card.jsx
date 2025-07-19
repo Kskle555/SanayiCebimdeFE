30f3e92fdbfd3d89d7057bf94e9c7fe18e18e544
Oguzhan Bilgi <oguzhanbilgi35@gmail.com>
2025-07-19 20:14:16 +0300

    git icin eklendi

--- src/app/components/ui/card.jsx
+++ src/app/components/ui/card.jsx
@@ -1,10 +1,11 @@
 // components/ui/card.js
 import React from "react";
-
+// test
 export const Card = ({ children, className = "" }) => {
   return <div className={`bg-white shadow-lg rounded-md ${className}`}>{children}</div>;
 };
 
 export const CardContent = ({ children, className = "" }) => {
   return <div className={`p-6 ${className}`}>{children}</div>;
+  
 };
