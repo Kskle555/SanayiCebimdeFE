2fabf693aae6b2131d6d28a3ae11b7869345c309
Oguzhan Bilgi <oguzhanbilgi35@gmail.com>
2025-08-25 21:47:43 +0300

    endpoint degistiirldi

--- src/app/store/useUstalarStore.jsx
+++ src/app/store/useUstalarStore.jsx
@@ -10,7 +10,7 @@ const useUstalarStore = create((set) => ({
   fetchUstalar: async () => {
     set({ isLoading: true, error: null })
     try {
-      const res = await axios.get('https://localhost:7173/ustalar') // ←
+      const res = await axios.get('https://api.sanayicebimde.net/api/ustalar') // ←
       set({ ustalar: res.data, isLoading: false })
     } catch (err) {
       set({ error: err.message || 'Veri çekme hatası', isLoading: false })
